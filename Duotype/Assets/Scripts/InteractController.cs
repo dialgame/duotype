b0ed8f12b919b1d551ac956f4236ecb6f0163e8e
Viktor <viktormmeliassen@gmail.com>
2023-01-12 23:42:46 +0100

    Interact

---
+++ Duotype/Assets/Scripts/InteractController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractController : MonoBehaviour
+{
+    public bool isOpen;
+    public SpriteRenderer sr;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    public void OpenChest()
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+            sr.color = Color.green;
+            Debug.Log("Chest is open");
+        }
+    }
+}
---
+++ Duotype/Assets/Scripts/Interactable.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInRange) //If we're in range to interact
+        {
+            if(Input.GetKeyDown(interactKey)) // And Player presses key
+            {
+                interactAction.Invoke(); // Fire Event
+            }
+        }
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+            Debug.Log("Player is in range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isInRange = false;
+        Debug.Log("Player is not in range");
+    }
+}
